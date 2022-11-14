0f1f7b88c6e3e55c4ecf144a279e838d8288b215
Arni Magnusson <thisisarni@gmail.com>
2022-11-14 18:09:20 +1100

    Add sra.R

---
+++ R/sra.R
@@ -0,0 +1,51 @@
+## Utility function to browse sraplus version history,
+## in terms of dependencies and files in the R directory
+
+sra <- function(sha, list=FALSE)
+{
+  ## Switch repository to SHA commit
+  cmd <- paste("git reset --hard", sha)
+  system(cmd, ignore.stdout=TRUE)
+
+  ## Date
+  dat <- system('git show -s --format=%cd --date=format:"%Y-%m-%d"',
+                intern=TRUE)
+
+  ## Version
+  ver <- read.dcf("DESCRIPTION", fields="Version")
+  ver <- unname(drop(ver))
+
+  ## Imports
+  imp <- read.dcf("DESCRIPTION", fields="Imports")
+  imp <- unname(drop(imp))
+  imp <- gsub("\\n", "", imp)
+  imp <- gsub(" ", "", imp)
+  imp <- gsub("\\(.*?\\)", "", imp)
+  imp <- unlist(strsplit(imp, ","))
+  imp <- sort(imp)
+
+  ## R directory
+  r <- dir("R")
+  r <- tools::file_path_sans_ext(r)
+
+  if(list)
+  {
+    out <- list(date=dat, version=ver, imports=imp, rfiles=r)
+  }
+  else
+  {
+    impstr <- paste0("(", length(imp), ") ", paste(imp, collapse=", "))
+    rstr <- paste0("(", length(r), ") ", paste(r, collapse=", "))
+    out <- c(date=dat, version=ver, import=impstr, rfiles=rstr)
+  }
+
+  out
+}
+
+################################################################################
+
+## tab <- readLines("../sraplus_history.md")
+## tab <- tab[16:77]
+## tab <- substring(tab, 1, 7)
+
+## x <- sapply(tab, sra)
